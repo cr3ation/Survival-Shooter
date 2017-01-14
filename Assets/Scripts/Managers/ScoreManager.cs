0e013fb987a3ca93fb2990b9c63e87b23e199e1a
Henrik Engström <me@henrikengstrom.com>
2017-01-15 00:17:36 +0100

    Score manager
    See your score! Hurray!

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -4,21 +4,25 @@ using System.Collections;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int score;
+    public static int score;        // The player's score.
 
 
-    Text text;
+    Text text;                      // Reference to the Text component.
 
 
-    void Awake ()
+    void Awake()
     {
-        text = GetComponent <Text> ();
+        // Set up the reference.
+        text = GetComponent<Text>();
+
+        // Reset the score.
         score = 0;
     }
 
 
-    void Update ()
+    void Update()
     {
+        // Set the displayed text to be the word "Score" followed by the score value.
         text.text = "Score: " + score;
     }
 }
