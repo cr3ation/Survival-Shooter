e8093be1275af3e8693625fe5502c8a7fe61f1e0
Henrik Engström <me@henrikengstrom.com>
2017-01-28 21:35:06 +0100

    Hit-animation added
    Hit-animation is looping when mouse1 is pressed. Lovisa stands still
    when mouse1 is pressed.

---
+++ Assets/Scripts/Lovisa/LovisaPunching.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class LovisaPunching : MonoBehaviour
+{
+    public int damagePerPunch = 20;                 // The damage inflicted by each bullet.
+    public float timeBetweenPunches = 0.15f;        // The time between each shot.
+    public float range = 50f;                       // The distance the gun can fire.
+
+    float timer;                                    // A timer to determine when to fire.
+    int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+    Animator anim;
+    LovisaMovement lovisaMovement;
+
+    void Awake()
+    {
+        // Create a layer mask for the Shootable layer.
+        shootableMask = LayerMask.GetMask("Shootable");
+        anim = GetComponent<Animator>();
+        lovisaMovement = GetComponent<LovisaMovement>();
+    }
+
+    void Update()
+    {
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the Fire1 button is being press and it's time to fire...
+        if (Input.GetButton("Fire1") && timer >= timeBetweenPunches)
+        {
+            // ... punch the enemy.
+            Shoot();
+        }
+        if (!Input.GetButton("Fire1"))
+        {
+            anim.SetBool("IsPunching", false);
+        }
+    }
+
+    void Shoot()
+    {
+        // Reset the timer.
+        timer = 0f;
+ 
+        anim.SetBool("IsPunching", true);
+
+        // Play the gun shot audioclip.
+        // gunAudio.Play();
+
+
+        // Stop the particles from playing if they were, then start the particles.
+        //gunParticles.Stop();
+        //gunParticles.Play();
+    }
+}
