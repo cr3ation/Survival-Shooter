c7a6a7fa4695692c5bcdbaf2989931fe90edfa4a
Henrik Engström <me@henrikengstrom.com>
2017-02-21 22:51:04 +0100

    Gamle restart after game over.
    Public int added to Game Over Manager-script, used in HUDcanvas.
    
    This is later on made to load random video, and then restart the game
    from level 1.

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -2,23 +2,38 @@
 
 public class GameOverManager : MonoBehaviour
 {
-    public FantonHealth fantonHealth;
+    public FantonHealth playerHealth;       // Reference to the player's health.
+    public float restartDelay = 5f;         // Time to wait before restarting the level
 
 
-    Animator anim;
+    Animator anim;                          // Reference to the animator component.
+    float restartTimer;                     // Timer to count up to restarting the level
 
 
     void Awake()
     {
+        // Set up the reference.
         anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
-        if (fantonHealth.currentHealth <= 0)
+        // If the player has run out of health...
+        if (playerHealth.currentHealth <= 0)
         {
+            // ... tell the animator the game is over.
             anim.SetTrigger("GameOver");
+
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if (restartTimer >= restartDelay)
+            {
+                // .. then reload the currently loaded level.
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
         }
     }
 }
