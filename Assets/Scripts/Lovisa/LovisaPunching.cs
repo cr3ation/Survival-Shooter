3ceb706d64360bc09ce6d4b4de59f441dfd74a1b
Albin Bergström <albinbergstrom@gmail.com>
2017-02-22 13:01:56 +0100

    Merge remote-tracking branch 'refs/remotes/origin/master' into Albin
    # Conflicts:
    #	Assets/Scenes/Level 01.unity
    #	Library/CurrentLayout.dwlt
    #	Library/InspectorExpandedItems.asset
    #	Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Library/UnityAssemblies/UnityEditor.Advertisements.dll
    #	Library/UnityAssemblies/UnityEditor.Analytics.dll
    #	Library/UnityAssemblies/UnityEditor.EditorTestsRunner.dll
    #	Library/UnityAssemblies/UnityEditor.Graphs.dll
    #	Library/UnityAssemblies/UnityEditor.HoloLens.dll
    #	Library/UnityAssemblies/UnityEditor.Networking.dll
    #	Library/UnityAssemblies/UnityEditor.OSXStandalone.Extensions.dll
    #	Library/UnityAssemblies/UnityEditor.PlaymodeTestsRunner.dll
    #	Library/UnityAssemblies/UnityEditor.TreeEditor.dll
    #	Library/UnityAssemblies/UnityEditor.UI.dll
    #	Library/UnityAssemblies/UnityEditor.VR.dll
    #	Library/UnityAssemblies/UnityEditor.WindowsStandalone.Extensions.dll
    #	Library/assetDatabase3
    #	Library/expandedItems
    #	Library/metadata/00/00000000000000004000000000000000
    #	Library/metadata/54/548c56f4bcf4db24b8aa057e9846a005.info

--- Assets/Scripts/Lovisa/LovisaPunching.cs
+++ Assets/Scripts/Lovisa/LovisaPunching.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class LovisaPunching : MonoBehaviour
 {
     public int damagePerPunch = 20;                 // The damage inflicted by each bullet.
-    public float timeBetweenPunches = 0.15f;        // The time between each shot.
+    public float timeBetweenPunches = 0.2f;        // The time between each shot.
     public float range = 3f;                        // The distance the gun can fire.
     public float rotationRange = 4f;                // The distance to an enemy when player starts to rotate
 
 
     float timer;                                    // A timer to determine when to fire.
+    float anim_timer;                               // A timer to control the punch animations.
+    bool is_punching = false;                       // True during the punch
     float distanceToEnemy;                          // Distance to the closest enemy
     float rotationSpeed = 10f;                      // Speen in witch to rotate
     Animator anim;                                  // Reference to the anomator controller object
@@ -24,12 +27,14 @@ public class LovisaPunching : MonoBehaviour
         anim = GetComponent<Animator>();
         lovisaMovement = GetComponent<LovisaMovement>();
         rigidBody = GetComponent<Rigidbody>();
+        Cursor.visible = false;
     }
 
     void Update()
     {
         // Add the time since Update was last called to the timer.
         timer += Time.deltaTime;
+        anim_timer += Time.deltaTime;
 
         // Find closest enemy
         closestEnemy = FindClosestEnemy();
@@ -45,6 +50,7 @@ public class LovisaPunching : MonoBehaviour
             distanceToEnemy = 10000000f;
         }
 
+        /*
         // Rotate player toward enemy if nessecery 
         Rotate();
 
@@ -62,36 +68,84 @@ public class LovisaPunching : MonoBehaviour
         if (!Input.GetButton("Fire1"))
         {
             anim.SetBool("IsPunching", false);
+        }*/
+
+        // If the punch-button is pressed and a punch is not performed at the moment
+        if (Input.GetKey("space") && !is_punching)
+        {
+            Shoot(false);
+            is_punching = true;
+            lovisaMovement.speed = 2f;
+            anim.SetBool("IsPunching", true);
+        }
+        // If a punching animation is already performed
+        else if (Input.GetKey("space") && timer >= timeBetweenPunches)
+        {
+            Shoot(true);
+        }
+
+        // Start the punching animation
+        if (anim_timer < 0.2f && is_punching)
+        {
+            anim.SetLayerWeight(1, Mathf.Lerp(0, 1, anim_timer * 5));
+        }
+        // End the punching animation
+        else if (anim_timer > 0.6)
+        {
+            anim.SetLayerWeight(1, Mathf.Lerp(1, 0, (anim_timer - 0.6f) * 5));
+
+            is_punching = false;
+            lovisaMovement.speed = 6;
+            anim.SetBool("IsPunching", false);
         }
     }
 
-    void Shoot()
+    void Shoot(bool only_damage)
     {
         // Play the gun shot audioclip.
         // gunAudio.Play();
-        // Reset the timer.
+        // Reset the animation timer.
+        if (!only_damage)
+        {
+            if (is_punching)
+                anim_timer = 0.2f;
+            else
+                anim_timer = 0f;
+        }
+
+        // Reset the punching (damage) timer.
         timer = 0f;
 
-        anim.SetBool("IsPunching", true);
+        //anim.SetBool("IsPunching", true);
 
-        // Try and find an EnemyHealth script on the gameobject hit.
-        EnemyHealth enemyHealth = closestEnemy.GetComponent<EnemyHealth>();
-
-        // If the EnemyHealth component exist...
-        if (enemyHealth != null)
+        if (distanceToEnemy <= range)
         {
-            // ... the enemy should take damage.
-            // TODO: change hitpoint to be where the actual hit was (hight and place)
-            Vector3 hitPoint = closestEnemy.transform.position;
-            hitPoint.y = 1.2f;
-            enemyHealth.TakeDamage(damagePerPunch, hitPoint);
+            // Only do damage if the enemy is in front of the player. 
+            Vector3 enemy_direction = Vector3.Normalize(closestEnemy.transform.position - rigidBody.transform.position);
+            Vector3 player_direction = Vector3.Normalize(rigidBody.transform.forward);
+            float angle = Mathf.Acos(Vector3.Dot(enemy_direction, player_direction));
+            if (angle < 3.14 / 3.0)
+            {
+                // Try and find an EnemyHealth script on the gameobject hit.
+                EnemyHealth enemyHealth = closestEnemy.GetComponent<EnemyHealth>();
+
+                // If the EnemyHealth component exist...
+                if (enemyHealth != null)
+                {
+                    // ... the enemy should take damage.
+                    // TODO: change hitpoint to be where the actual hit was (hight and place)
+                    Vector3 hitPoint = closestEnemy.transform.position;
+                    hitPoint.y = 1.2f;
+                    enemyHealth.TakeDamage(damagePerPunch, hitPoint);
+                }
+            }
         }
     }
 
     /// <summary>
     /// Rotates player towards the closest enemy if needed.
     /// </summary>
-    void Rotate()
+    /*void Rotate()
     {
         // If punching animation is activated. Rotate toward cloases enemy
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("Punch") && distanceToEnemy <= rotationRange)
@@ -101,7 +155,7 @@ public class LovisaPunching : MonoBehaviour
             var rotation = Quaternion.LookRotation(lookPos);
             rigidBody.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
-    }
+    }*/
 
     // Find the name of the closest enemy
     GameObject FindClosestEnemy()
