2f77f90ca049fac60f9e935651884db7bf28ffb9
Henrik Engström <me@henrikengstrom.com>
2017-03-04 12:40:33 +0100

    Leaderboard working at home scren.

---
+++ Assets/Scripts/Managers/LeaderboardManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardManager : MonoBehaviour {
+
+    public GameObject dreamloPrefab;
+    public Text playerNamesList;
+    public Text scoresList;
+
+    private bool scoreHasLoaded = false;
+    private dreamloLeaderBoard leaderBoard;
+    private List<dreamloLeaderBoard.Score> highscores;
+
+	// Use this for initialization
+	void Start () {
+
+        leaderBoard = dreamloPrefab.GetComponent<dreamloLeaderBoard>();
+        leaderBoard.LoadScores();
+
+        playerNamesList.text = "";
+        scoresList.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (scoreHasLoaded) return;
+        if (leaderBoard.ToListHighToLow().Count > 0)
+        {
+            scoreHasLoaded = true;
+            highscores = leaderBoard.ToListHighToLow();
+            ShowScoreOnLeaderboard();
+        }		
+	}
+
+    private void ShowScoreOnLeaderboard()
+    {
+        var scoresToShow = 8;
+        var i = 0;
+        foreach (var score in highscores)
+        {
+            playerNamesList.text += score.playerName + "\n";
+            scoresList.text += score.score + "\n";
+            i++;
+            if (i == scoresToShow) break;
+        }
+    }
+}
