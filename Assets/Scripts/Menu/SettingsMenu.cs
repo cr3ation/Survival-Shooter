3cc1523fe680ddaea12793a9d7fe7d13e2f90171
Henrik Engström <me@henrikengstrom.com>
2017-03-06 16:07:56 +0100

    Possible to change graphics settings
    Check out the quality-feature!

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -18,8 +18,21 @@ public class SettingsMenu : MonoBehaviour {
         Resolution[] resolutions = Screen.resolutions;
         foreach (Resolution res in resolutions)
         {
-            resolution.options.Add(new Dropdown.OptionData() { text = res.width + "x" + res.height });
+            if (res.width > 1024)
+            {
+                resolution.options.Add(new Dropdown.OptionData() { text = res.width + "x" + res.height });
+            }
         }
+        Resolution currentResolution = Screen.currentResolution;
+        for (int i = 0; i < resolution.options.Count; i++)
+        {
+            var res = resolution.options[i].text.Split('x');
+            if (currentResolution.width == int.Parse(res[0]) && currentResolution.height == int.Parse(res[1]))
+            {
+                resolution.value = i;
+            }
+        }
+
 
         // Graphics quality drop down
         string[] qualities = QualitySettings.names;
@@ -27,11 +40,11 @@ public class SettingsMenu : MonoBehaviour {
         {
             quality.options.Add(new Dropdown.OptionData() { text = qual });
         }
+        quality.value = QualitySettings.GetQualityLevel();
 
         // Window mode
         windowed.isOn = !Screen.fullScreen;
 
-        quality.value = QualitySettings.GetQualityLevel();
         quality.RefreshShownValue();
         resolution.RefreshShownValue();
     }
