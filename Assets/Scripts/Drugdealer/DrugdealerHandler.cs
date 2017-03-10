5ad6b94d7d81ed38f34c448c4fa80b59d9a800f6
albin.bergstrom <albin.bergstrom@est.fib.upc.edu>
2017-03-10 13:53:47 +0100

    Added a drugdealer
    The drugdealers position is randomized at game start. When Lovisa walks
    close to him Fanton recieves some drugs.

---
+++ Assets/Scripts/Drugdealer/DrugdealerHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrugdealerHandler : MonoBehaviour {
+
+    public GameObject spawnPoints;
+    private bool hasDrugs = true;
+
+	// Use this for initialization
+	void Start () {
+        // Randomize the starting position.
+        int spawnAt = (int)Mathf.Round(Random.Range(0, spawnPoints.transform.childCount));
+        print(spawnAt);
+        transform.position = spawnPoints.transform.GetChild(spawnAt).position;
+        transform.rotation = spawnPoints.transform.GetChild(spawnAt).rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Lovisa enters the collider, give the player some drugs.
+        if(other.name == "Lovisa" && hasDrugs)
+        {
+            FantonHealth.haveDrugs = true;
+            hasDrugs = false;
+        }
+    }
+}
