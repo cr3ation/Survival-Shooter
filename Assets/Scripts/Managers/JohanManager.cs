cbb08e122296854326f0eef67c16835a059c5f1b
Albin Bergström <albinbergstrom@gmail.com>
2017-03-14 17:46:33 +0100

    Added Johan
    Johan now spawns at different locations.
    When getting close to Johan a shirt is recieved and he becomes naked and
    sad.
    The inventory-bar is updated as well as Fantons health-script.

---
+++ Assets/Scripts/Managers/JohanManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohanManager : MonoBehaviour {
+
+    public GameObject spawnPoints;
+    private bool hasShirt = true;
+    private Animator anim;
+
+
+    // Use this for initialization
+    void Start () {
+        // Randomize the starting position.
+        int spawnAt = (int)Mathf.Round(Random.Range(0, spawnPoints.transform.childCount));
+        print("Johan spawnpos: " + spawnAt);
+        transform.position = spawnPoints.transform.GetChild(spawnAt).position;
+        transform.rotation = spawnPoints.transform.GetChild(spawnAt).rotation;
+
+        anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Lovisa enters the collider, give the player a shirt.
+        if (other.name == "Lovisa" && hasShirt)
+        {
+            FantonHealth.haveShirt = true;
+            hasShirt = false;
+
+            // Start the sad animations.
+            anim.SetTrigger("LostShirt");
+
+            // Inactivate the object containing the shirt.
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
