5dcccae7ec3889b8923f55939147b27c1aba5fae
Henrik Engström <me@henrikengstrom.com>
2017-03-17 18:46:06 +0100

    Pause menu added

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PauseMenu : MonoBehaviour {
+
+    // public Slider audioSlider;
+    Slider audioSlider;
+	
+    // Use this for initialization
+	void Start () {
+        Cursor.visible = true;
+        audioSlider = transform.GetComponentInChildren<Slider>();
+        audioSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string newGameLevel)
+    {
+        if (string.IsNullOrEmpty(newGameLevel))
+        {
+            return;
+        }
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    /// <summary>
+    /// Used to set background volume
+    /// </summary>
+    public void SetBackgroundMusicVolume()
+    {
+        var audioSource = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+        audioSource.volume = audioSlider.value;
+        PlayerPrefs.SetFloat("Volume", audioSlider.value);
+    }
+}
