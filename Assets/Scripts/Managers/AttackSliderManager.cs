658477e9217020894e0dda3ff88c66080d4d9397
Albin Bergström <albinbergstrom@gmail.com>
2017-03-17 22:51:18 +0100

    Added flashing broken things

---
+++ Assets/Scripts/Managers/AttackSliderManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackSliderManager : MonoBehaviour {
+
+    public Sprite attackActive;
+    public Sprite attackNormal;
+    public Image image;
+    public Slider slider;
+
+    Animator animator;
+    bool hasChangedImage = false;
+
+    // Use this for initialization
+    void Start () {
+        animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Prevent the animation trigger from constantly getting triggered.
+        if (slider.value > 99.5f && !hasChangedImage)
+        {
+            hasChangedImage = true;
+            image.sprite = attackActive;
+            animator.SetTrigger("AttackReady");
+            print("HEJJ");
+        }
+        else if(slider.value < 90)
+        {
+            hasChangedImage = false;
+            image.sprite = attackNormal;
+        }
+    }
+}
--- Assets/Scripts/Managers/PunchSliderManager.cs
+++ Assets/Scripts/Managers/PunchSliderManager.cs
@@ -8,23 +8,33 @@ public class PunchSliderManager : MonoBehaviour {
     public Sprite punchActive;
     public Sprite punchNormal;
 
+    Animator animator;
+    Slider slider;
+    float lastSliderValue;
 
 	// Use this for initialization
 	void Start () {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        slider = transform.GetChild(2).GetComponent<Slider>();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(transform.GetChild(1).GetComponent<Slider>().value > 99.5f)
+        lastSliderValue = slider.value;
+
+        // Prevent the animation trigger from constantly getting triggered.
+        if(slider.value > 99.5f && slider.value != lastSliderValue)
         {
-            transform.GetChild(0).GetComponent<Image>().sprite = punchActive;
+            transform.GetChild(1).GetComponent<Image>().sprite = punchActive;
+            animator.SetTrigger("AttackReady");
         }
         else
         {
-            transform.GetChild(0).GetComponent<Image>().sprite = punchNormal;
+            transform.GetChild(1).GetComponent<Image>().sprite = punchNormal;
         }
+
 
     }
 }
